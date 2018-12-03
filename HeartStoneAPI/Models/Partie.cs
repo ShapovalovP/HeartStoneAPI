25544adca408dbca36d243d2f2bc380d04522bf7
Shapovalov Pavlo <1409770@cegepmontpetit.ca>
2018-12-03 11:30:19 -0500

    Ajoutez des fichiers projet.

---
+++ HeartStoneAPI/Models/Partie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeartStoneAPI.Models
+{
+    public class Partie
+    {
+        public int Id { get; set; }
+      
+
+        public virtual List<ApplicationUser> Users { get; set; }
+        public virtual List<Deck> Decks { get; set; }
+        public virtual List<Tour> Tours { get; set; }
+    }
+}
---
+++ HeartStoneAPI/Models/Tour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeartStoneAPI.Models
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+
+        public int PartieId { get; set; }
+        public virtual List<Carte> Cartes { get; set; }
+      
+    }
+}
