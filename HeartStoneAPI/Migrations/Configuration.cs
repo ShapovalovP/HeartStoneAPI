bd30c0f014567b06b1b08a5d2dc8d96b631a5ec4
Gabriel Lanoville <gaballive@hotmail.com>
2018-12-07 14:01:58 -0500

    Changement des cartes

--- HeartStoneAPI/Migrations/Configuration.cs
+++ HeartStoneAPI/Migrations/Configuration.cs
@@ -28,15 +28,24 @@ namespace HeartStoneAPI.Migrations
 
             List<Carte> cartes = new List<Carte>()
             {
-                new Carte {ValeurAttaque = 1, ValeurDefense =2, prixAchat = 0.9M , prixVendre= 1M, imageDerier = "/assets/der.jpg", image= "/assets/feu.jpg" },
-                new Carte {ValeurAttaque = 2, ValeurDefense =3, prixAchat = 1M , prixVendre= 1.1M, imageDerier = "/assets/der.jpg", image= "/assets/water.jpg" },
-                new Carte {ValeurAttaque = 3, ValeurDefense =4, prixAchat = 1.1M , prixVendre= 1.2M, imageDerier = "/assets/der.jpg", image= "/assets/terre.jpg" },
-                new Carte {ValeurAttaque = 4, ValeurDefense =5, prixAchat = 1.2M , prixVendre= 1.3M, imageDerier = "/assets/der.jpg", image= "/assets/feu.jpg" },
-                new Carte {ValeurAttaque = 5, ValeurDefense =6, prixAchat = 1.3M , prixVendre= 1.4M, imageDerier = "/assets/der.jpg", image= "/assets/water.jpg" },
-                new Carte {ValeurAttaque = 6, ValeurDefense =7, prixAchat = 1.4M , prixVendre= 1.5M, imageDerier = "/assets/der.jpg", image= "/assets/terre.jpg" },
-                new Carte {ValeurAttaque = 7, ValeurDefense =8, prixAchat = 1.5M , prixVendre= 1.6M, imageDerier = "/assets/der.jpg", image= "/assets/feu.jpg" },
-                new Carte {ValeurAttaque = 8, ValeurDefense =9, prixAchat = 1.6M , prixVendre= 1.7M, imageDerier = "/assets/der.jpg", image= "/assets/water.jpg" },
-                new Carte {ValeurAttaque = 9, ValeurDefense =10, prixAchat =1.7M , prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/terre.jpg" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/alphys.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/asgore.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/asriel.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/burgerpants.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/chara.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/dog.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/dummy.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/flowey.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/frisk.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/gaster.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/mettaton.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/muffet.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/napstablook.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/papyrus.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/sans.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/temmie.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/Toriel.png" },
+                new Carte { ValeurAttaque = 10, ValeurDefense=10, prixAchat = 1.7M, prixVendre= 1.8M, imageDerier = "/assets/der.jpg", image= "/assets/undyne.png" },
             };
            /*  List< ApplicationUser > users = new List<ApplicationUser>()
              {
